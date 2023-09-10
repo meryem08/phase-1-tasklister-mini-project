1ec17f49ac37e61413dcaa35568b113d22356efb
meryem08 <meriem.mansouri08@gmail.com>
2023-09-10 22:43:08 +0100

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById("new-task-description").value;
+
+    if (taskDescription) {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => {
+        taskItem.remove();
+      });
+
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+
+      form.reset();
+    }
+  });
 });
